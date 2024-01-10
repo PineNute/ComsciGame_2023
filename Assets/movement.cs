418f10c93392fc6046cf0ab2658b0e9b66caed3d
PineNute <144453225+PineNute@users.noreply.github.com>
2024-01-10 09:22:40 -0500

    Update movement.cs

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -9,17 +9,19 @@ using UnityEngine.UI;
 
 public class movement : MonoBehaviour
 {
+//Creates the user as a rigid body
     Rigidbody rb;
      [Header("Movement")]
      public float groundDrag;
 
-
+//lets the user check for the ground
      [Header("Ground Check")]
      public float playerHeight;
      public LayerMask whatIsGround;
      bool grounded;
      bool jump;
 
+//Declares variables for mvoement
       public float movespeed;
       public Transform orientation;
       float horizontalInput;
@@ -29,7 +31,7 @@ public class movement : MonoBehaviour
       public float jumpCooldown;
       public float airMultiplier;
       bool readyToJump;
-
+//Declares keybinds
       [Header("Keybinds")]
       public KeyCode jumpkey = KeyCode.Space;
 
@@ -45,6 +47,7 @@ public class movement : MonoBehaviour
 
 void Update()
     {
+    //Basic debug program
       Debug.Log(grounded);
       MyInput();
       grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight, whatIsGround);
@@ -53,7 +56,7 @@ void Update()
       }else{
         rb.drag = 0;
       }
-
+//Enables the user to jump
       jump = Physics.Raycast(transform.position, Vector3.down, playerHeight, whatIsGround);
   
       if(jump == true && Input.GetKeyDown(KeyCode.Space)){
@@ -66,6 +69,7 @@ void Update()
     private void FixedUpdate(){
       MovePlayer();
     }
+    //Code to decide if user is able to jump
     private void MyInput(){
       horizontalInput = Input.GetAxisRaw("Horizontal");
       verticalInput = Input.GetAxisRaw("Vertical");
@@ -77,23 +81,25 @@ void Update()
         Invoke(nameof(ResetJump), jumpCooldown);
       }
     }
-    
+    // Code that enables the player to move diagonally in the air
     private void MovePlayer(){
       moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
       if(grounded){
+        // Code that enables the player to move diagonally in the air
+      
         rb.AddForce(moveDirection.normalized * movespeed * 10f, ForceMode.Force);
       }else if(!grounded){
          rb.AddForce(moveDirection.normalized * movespeed * 10f * airMultiplier, ForceMode.Force);
       }
       
     }
-
+// Code that decides what happens to the user
     private void Jump(){
       rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.y); 
       rb.AddForce(transform.up * jumpforce, ForceMode.Impulse);
     }
-    
+    // Code that resets the jump after the user has jumped 
     private void ResetJump(){
       readyToJump = true;
     }
